00641f4513115853ee452f9b0051a21cc683b988
Natalia Oliveira Ribeiro <91141994+NatiRibei@users.noreply.github.com>
2022-08-30 13:52:44 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,45 @@
 
 var chao1, teto, cubo, cuboImage, cubo2, flor;
 var grupo, pointGroup;
-var song, camera;
+var song, cam;
 
 //Função Pre-load
 function preLoad (){
   song = loadSound ("song18.mp3");
-  cuboImage = loadImage ("cubo.gif");
-  cubo2 = loadImage ("cuboExplode.gif");
-  flor = loadImage ("pts1.gif");
+  //cuboImage = loadImage ("cubo.gif");
+  //cubo2 = loadImage ("cuboExplode.gif");
+  //flor = loadImage ("pts1.gif");
 }
 
 
 
 // Função Setup
-function Setup(){
+function setup() {
+  createCanvas (600,400, WEBGL);
+  //câmera ligada
+  cam = createCamera ();
+ 
   //criar sprites
-  chao1 = rectangle (200, 600, 600, 200);
+  
+  chao1 = createSprite (200, 500, 400, 200);
   chao1.shapeColor = "#aa686b";
   
-  teto = rectangle (200, 150, 600, 300);
+  teto = createSprite (200, 200, 600, 300);
   teto.shapeColor = "#aa686b";
   
-  cubo = createSprite(200,490);
-  cubo.addAnimation(cuboImage);
-  cubo.scale = 0.25;
+  cubo = createSprite(200,395,50,50);
+  cubo.addAnimation("cuboImage","cubo.gif");
+  cubo.addAnimation("cuboExplode","cuboExplode.gif");
+  cubo.scale = 0.05;
   
   //criar grupos
   pointGroup = new Group();
   grupo = new Group();
-
-  //câmera ligada
-  camera = createCamera ();
+  
+  
 
   //subindo som
-  
+  //song.play();
 }
 
 //criar variáveis de pontuação
@@ -43,17 +48,17 @@ var pontoC=0;
 //Variável de posição x do sprite para aumento
 var spriteXval=5;
 
-//tocar som de jogo
-//song.play();
 //Função DRAW
 function draw() {
   //Limpar tela
   background("black");
+  //move (cubo.x,cubo.y,0);
   //Fixar a câmera no cubo
-  //camera.move (cubo.x, cubo.y,0,0,0,0,0,0,0);
+  cam.setPosition(cubo.x, cubo.y,100);
   //Efeito de rolamento
-  //cubo.rotationSpeed =10;
+  cubo.rotationSpeed =10;
   //Aumentar a posição x para o sprite, para fazer com que pareça que está rolando
+  console.log(cubo);
   cubo.x = cubo.x + spriteXval;
   //Mudar piso e teto
   chao1.x = chao1.x + spriteXval;
@@ -64,12 +69,12 @@ function draw() {
     setTimeout(function() {
       cubo.velocityY = cubo.velocityY +10;
     }, 500);
-    playSound("sound://category_jump/vibrant_game_jumping_up_1.mp3", false);
+    //playSound("sound://category_jump/vibrant_game_jumping_up_1.mp3", false);
   }
   //Para evitar que o cubo caia abaixo do chão
   cubo.collide(chao1);
   //definir colisor do solo
-  chao1.setCollider("rectangle", 0, 3.5, 600, 180);
+  chao1.setCollider("rectangle", 0, -5, 395, 180);
   //Para evitar que o cubo vá para cima
   cubo.collide(teto);
   //Chamar função para colocar os obstáculos
@@ -77,21 +82,21 @@ function draw() {
   spawnPoints();
   //Quando sprite toca os obstáculos
   if(grupo.isTouching(cubo)){
-    cubo.addAnimation(cubo2);
+    cubo.changeAnimation("cuboExplode");
     cubo.x=cubo.x-200;
     chao1.x=chao1.x-200;
     teto.x=teto.x-200;
     setTimeout(function() {
-      cubo.addAnimation(cuboImage);
+      cubo.changeAnimation("cuboImage");
     }, 200);
-    playSound("sound://category_hits/vibrant_game_dirty_desolve_1.mp3", false);
+    //playSound("sound://category_hits/vibrant_game_dirty_desolve_1.mp3", false);
   }
   //Quando o sprite coleta pontos
   for (var i = 0;i<pontoC;i++){
   if (pointGroup.get(i) != undefined && pointGroup.get(i).isTouching(cubo))
     {
       pointGroup.get(i).destroy();
-      playSound("sound://category_collect/energy_bar_recharge_4.mp3", false);
+      //playSound("sound://category_collect/energy_bar_recharge_4.mp3", false);
       pontuacao=pontuacao+100;
     }
   }
@@ -99,18 +104,18 @@ function draw() {
   //Exibir pontuação
   fill("White");
   textSize(20);
-  text("Pontuação: "+pontuacao, cubo.x-200,380);
+  text("Pontuação: "+pontuacao, cubo.x+50,250);
   drawSprites();
   }
 //******************Função para gerar obstáculos*************************************
 function spawn(){
 //Variáveis de dimensão
-var distNum=randomNumber(30,100);
-var distNum2=randomNumber(30,100);
-var obsW=randomNumber(10,40);
-var obsH=randomNumber(10,40);
+var distNum=Math.round(random(30,100));
+var distNum2=Math.round(random(30,100));
+var obsW=Math.round(random(10,40));
+var obsH=Math.round(random(10,40));
 if(distNum == distNum2){
-var obs1 = createSprite(sprite.x+300, 490,obsW,obsH);
+var obs1 = createSprite(cubo.x+300, 395,obsW,obsH);
 obs1.shapeColor=chao1.shapeColor;
 obs1.lifetime=100;
 grupo.add(obs1);
@@ -118,13 +123,13 @@ grupo.add(obs1);
 }
 //********************Função para gerar pontos***********************************
 function spawnPoints(){
-var distNum = randomNumber(30,100);
-var distNum2 = randomNumber(30,100);
+var distNum = Math.round(random(30,100));
+var distNum2 = Math.round(random(30,100));
 if(distNum==distNum2){
-var pts1 = createSprite(sprite.x+300, randomNumber(400, 430),10,10);
-pts1.addAnimation(flor);
+var pts1 = createSprite(cubo.x+200, Math.round(random(100,300)),10,10);
+pts1.addAnimation("flor","pts1.gif");
 pts1.setCollider("rectangle", 0, 0,40, 40);
-pts1.scale = 0.2;
+pts1.scale = 0.05;
 pts1.lifetime=100;
 pointGroup.add(pts1);
 pontoC=pontoC+1;
